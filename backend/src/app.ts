90b03d36d0f49498c4ab9bdd52e6e11b0f19d423
Carlos Ibarra <tu_correo@ejemplo.com>
2026-06-05 23:18:00 -0500

    feat: CRUD de activos completado y corrección en detalle de extintores

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -13,6 +13,7 @@ import visitasRoutes from "./modules/visitas/visitas/visita.routes";
 import visitantesRoutes from "./modules/visitas/visitantes/visitante.routes";
 import incidentesRoutes from "./modules/incidentes/incidentes.routes";
 import sismosRoutes from "./modules/sismos/sismos.routes";
+import activoRoutes from "./modules/inventario/activo.routes";
 
 const app = express();
 /* ========================= */
@@ -37,6 +38,7 @@ app.use( "/visitas",visitasRoutes);
 app.use( "/visitantes",visitantesRoutes);
 app.use( "/incidentes",incidentesRoutes);
 app.use( "/sismos",sismosRoutes);
+app.use( "/", activoRoutes);
 
 app.use(
   "/uploads",
